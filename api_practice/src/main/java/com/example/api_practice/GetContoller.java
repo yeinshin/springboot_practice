a7848760302e7679ef866771758fc52741af7011
yein <syi05111@naver.com>
2022-07-12 23:41:33 +0900

    GetContoller 구현
    - 매개변수가 없는 GET 메서드 구현
    - using @GetMapping

--- api_practice/src/main/java/com/example/api_practice/GetContoller.java
+++ api_practice/src/main/java/com/example/api_practice/GetContoller.java
@@ -16,7 +16,7 @@ public class GetContoller {
     }
 
     /*
-    매개변수가 없는 GET 메서드 구현
+       매개변수가 없는 GET 메서드 구현
         - Spring 4.3 버전 이후로는 새로 나온 어노테이션을 사용하기 때문에 @RequestMapping 어노테이션은 더 이상 사용되지 않는다.
         - Ex)
             - @GetMapping
