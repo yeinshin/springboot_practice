9f33862fa983ebf83ab2a9115224fa11c9e9fba8
yein <syi05111@naver.com>
2022-07-14 09:53:02 +0900

    PutController 구현
    - ResponseEntity를 활용한 PUT 메서드 구현

--- api_practice/src/main/java/com/example/api_practice/controller/PutController.java
+++ api_practice/src/main/java/com/example/api_practice/controller/PutController.java
@@ -1,6 +1,8 @@
 package com.example.api_practice.controller;
 
 import com.example.api_practice.dto.MemberDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -50,6 +52,17 @@ public class PutController {
     @PutMapping(value = "/member2")
     public MemberDto postMemberDto2(@RequestBody MemberDto memberDto){
         return memberDto;
+    }
+
+    /*
+       ResponseEntity를 활용한 PUT 메서드 구현
+        - RequestEntity와 ResponseEntity는 HttpEntity를 상속받아 구현한 클래스.
+        - ResponseEntity : 서버에 들어온 요청에 대해 응답 데이터를 구성해서 전달할 수 있게 함.
+        - ResponseEntity는 HttpEntity로부터 HttpHeaders와 Body를 가지고 자체적으로 HttpStatus를 구현함.
+     */
+    @PutMapping(value = "/member3")
+    public ResponseEntity<MemberDto> postMemberDto3(@RequestBody MemberDto memberDto){
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(memberDto);
     }
 
 }
